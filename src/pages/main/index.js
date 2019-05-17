a246be43ae60b526f464af98d53d836329d7bf08
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2019-05-17 18:09:37 -0300

    Added paginate product info resource

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,21 +5,45 @@ import './styles.css';
 
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productInfo: {},
+    page: 1
   }
 
   componentDidMound() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products');
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page={page}`);
 
-    this.setState({ products: response.data.docs });
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page });
+  }
+
+  prevPage = () => {
+    const { page } = this.state;
+
+    if(page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  }
+
+  nextPage = () => {
+    const { productInfo, page } = this.state;
+
+    if(page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
   }
 
   render() {
-    const { products } = this.state;
+    const { products, productInfo, page } = this.state;
 
     return (
       <div className="product-list">
@@ -31,6 +55,10 @@ export default class Main extends Component {
             <a href="">Access</a>
           </article>
         ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>Previous</button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>Next</button>
+        </div>
       </div>
     );
   }
